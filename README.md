3429750d5d8284a7b52726c577a941e85d3ce3c2
julianromera <julian.romera@gmail.com>
2014-02-21 05:44:24 +0100

    uploading distribution

--- README.md
+++ README.md
@@ -1,4 +1,61 @@
-agronet-full-distro
-===================
+###[agronet](#)
 
-Agronet's full distribution
+(not working yet)
+Agronet's Drupal Installation Profile
+
+###Demo Prototype
+
+A current live snapshot can be seen on [on this link](http://agronet.appgee.net)
+
+###Requirements
+
+- xamp(*) (Debian GNU/Linux preferred)
+- Git
+- Tar
+- Curl
+- [Drush](//github.com/drush-ops/drush) `>= version 6.x``
+
+
+(*) x=(Windows/Linux/Mac OS)-(Apache/Nginx)-MySQL-PHP
+
+    
+###Installation  
+
+
+(Tested to work on Debian 7 wheezy)
+
+```
+
+```
+     
+
+###Resources
+
+- [Drupal 7](https://drupal.org/drupal-7.0)
+- [Drupal Commons 3](http://www.acquia.com/demo-drupal-commons-3)
+- [NGINX]() / [Apache2]() + [PHP5 FastCGI]() + [MySQL]() + [MongoDB]()
+- [Apache Solr](http://lucene.apache.org/solr/)
+- [Memcached](http://memcached.org/)
+- [APC](http://en.wikipedia.org/wiki/List_of_PHP_accelerators)
+- [Varnish](https://www.varnish-cache.org/)
+- [CDN](http://en.wikipedia.org/wiki/Content_delivery_network)
+- [Bootstrap 2](http://getbootstrap.com/2.3.2/)
+- [Honeypot Captcha](http://en.wikipedia.org/wiki/Honeypot_(computing))
+- Usability Tests
+- [Scalability Tests](https://github.com/julianromerajuarez/apachesolr-benchs)
+
+
+###Troubleshooting
+
+[TroubleShooting Wiki](https://github.com/julianromera/agronet/wiki/Troubleshooting)
+
+###Author
+
+University of Alcala
+
+###License
+
+Licensed under GPL/ GNU version 2 License. For more information, 
+see the accompanying LICENSE file.  
+
+
---
+++ _installer.sh
@@ -0,0 +1,139 @@
+#!/bin/bash
+
+# must start with '/'
+DESTINATION_DIR="/www/agronet_test1"
+DATABASE_NAME="agronet_test1"
+DATABASE_USER="root"
+
+DRUSH=`which drush`
+GIT=`which git` 
+MYSQLADMIN=`which mysqladmin`
+TAR=`which tar` 
+
+if [ "x$MYSQLADMIN" = "x" ];then
+    echo "An MySql Database, and access to mysqladmin command is required."
+    exit 1
+fi
+
+if [ "x$DRUSH" = "x" ];then
+    echo "Drush command not found. Drush version >= 6 is required."
+    exit 1
+fi
+
+if [ "x$GIT" = "x" ];then
+    echo "Git command not found. "
+    exit 1
+fi
+
+if [ "x$TAR" = "x" ];then
+    echo "Tar command not found. "
+    exit 1
+fi
+
+if [ "x$DESTINATION_DIR" = "x" ];then
+    echo "DESTINATION_DIR can not be empty."
+fi
+
+if [ -d $DESTINATION_DIR ];then
+    echo "Destination directory ($DESTINATION_DIR) should not exist. edit this file to change it."
+    exit 1
+fi
+
+
+install() {
+
+    cd /tmp
+
+    if [ -d /tmp$DESTINATION_DIR ];then
+      echo '/tmp$DESTINATION_DIR exists. Override it? (y/n)'
+      read yesno
+      
+      if [ "x$yesno" = "y" ];then
+          rm -rf /tmp$DESTINATION_DIR
+      else
+          echo "quiting.."
+          exit 0
+      fi
+      
+    fi
+    
+    echo "cloning remote respository.."
+    git clone https://github.com/julianromera/agronet.git /tmp$DESTINATION_DIR 
+
+    cd /tmp$DESTINATION_DIR
+    ./make-agronet.sh 
+
+    echo "copying recently created dir to destination.."
+    mkdir -p $DESTINATION_DIR 
+    cp -R /tmp$DESTINATION_DIR/* $DESTINATION_DIR 
+
+
+    echo "copying scripts to $DESTINATION_DIR.."
+    
+    cp /tmp$DESTINATION_DIR/*.sh $DESTINATION_DIR
+    res=$?
+    checkok $res
+    cp /tmp$DESTINATION_DIR/*.inc $DESTINATION_DIR
+    res=$?
+    checkok $res
+
+    echo -n 'enter your Drupal MySql user: '
+    read my_user
+
+    echo -n 'enter your Drupal MySql password: '
+    read my_passwd
+    
+    echo "Installing full site.. please be patient"
+    cd $DESTINATION_DIR
+    
+    drush site-install commons --account-name=admin --account-pass=admin --db-url=mysql:/$my_user:$my_passwd@localhost/$DATABASE_NAME
+    res=$?
+    checkok $res
+
+    echo "downloading latest database-dump"
+    curl -O  https://github.com/julianromera/agronet-database/raw/master/agronet-db.sql.tar
+    res=$?    
+    checkok $res
+        
+    tar -xzvf agronet-db.sql.tar
+    res=$?
+    checkok $res
+
+    echo "Doing modifications to Drupal Commons..."
+    cd $DESTINATION_DIR
+    ./conf-agronet.sh $DESTINATION_DIR ./agronet-db.sql 
+    res=$?
+    checkok $res
+    
+    echo "postinstalling..."
+    cd $DESTINATION_DIR_
+    ./postinstall $DESTINATION_DIR
+    res=$?
+    
+    checkok $res
+    
+}
+
+checkok() {
+    if [ $1 -ne 0 ];then
+        echo "Something went wrong. aborting.."
+        exit 1
+    fi
+}
+
+
+echo "creating database $DATABASE_NAME... enter mysql admin password when requested."
+sudo mysqladmin -uroot -p create $DATABASE_NAME
+res=$?
+
+checkok $res
+
+install
+ 
+drush --root=$DESTINATION_DIR cc all
+drush --root=$DESTINATION_DIR updb
+
+echo "Check that $DESTINATION_DIR/sites/default/settings.php contains the same database" 
+echo "credentials than you just created"
+
+
